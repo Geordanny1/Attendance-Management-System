5130331e64bf9ad8866520a9399027345a364b3d
Geordanny <geordanipinales@gmail.com>
2026-07-01 18:17:43 -0400

    change month functionalitty

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
@@ -28,7 +28,6 @@ def get_present_year_and_month() -> tuple[int, int]:
     import datetime as dt
 
     today = dt.date.today()
-
     year = today.year
     month = today.month
 
@@ -39,12 +38,95 @@ def get_present_month_calendar() -> str:
     import calendar
     
     cal = calendar.HTMLCalendar()
-
-
     year, month  =  get_present_year_and_month()
     present_month_calendar = cal.formatmonth(year, month, withyear = True) 
 
     return present_month_calendar
 
+@app.route("/changemonth", methods = ["POST"])
+def changemonth():
 
+    data = request.get_json()
+
+    year   = data.get("year")
+    month  = data.get("year")
+    new_date = get_new_date(data) 
+    new_year  = new_date["year"]
+    new_month = new_date["month"]
+
+    calendar = get_month_html_calendar(new_year, new_month)
+
+    data = {
+            "html" : calendar
+            }
+
+    return jsonify(data)
+
+
+
+
+def get_new_date(date: dict) -> dict[str, int]:
+
+    year  = date["year"]
+    month  = date["month"]
+    direction  = date["direction"]
+
+    if direction == "next":
+        if month == 12:
+
+            year  += 1
+            month  = 1
+            new_date = {
+                    "year" : year,
+                    "month" : month
+                    }
+
+            return new_date
+
+        month += 1
+        new_date = {
+                "year"  :  year,
+                "month" : month
+                }
+
+        return new_date
+
+    elif direction == "prev":
+        if month == 1:
+
+            month = 12
+            year -= 1
+            new_date = {
+                    "year":  year,
+                    "month": month
+                    }
+
+            return new_date
+
+        month -= 1
+        new_date = {
+                "month" : month,
+                "year"  : year
+                }
+
+        return new_date
+
+    elif direction == "present":
+
+        new_year, new_month = get_present_year_and_month()
+        new_date = {
+                "year":  new_year,
+                "month": new_month
+                }
+
+        return new_date
+
+
+def get_month_html_calendar(year: int, month: int) -> str:
+    import calendar
+
+    cal = calendar.HTMLCalendar()
+    html_calendar = cal.formatmonth(year, month, withyear = True) 
+
+    return html_calendar
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-(function inputCheck() {
+function inputCheck() {
 
   const dayEl = document.querySelectorAll("ul.day-list>li");
   dayEl.forEach( el => {
@@ -9,10 +9,11 @@
     })
   })
 
-})();
+};
 
 
-(function changeDayColorBaseOnInput() {
+
+function changeDayColorBaseOnInput() {
 
   const nodeMondays   = document.querySelectorAll(".mon");
   const nodeTuesadys  = document.querySelectorAll(".tue");
@@ -41,13 +42,13 @@
 
   mondayInput.addEventListener("click", el => {
     mondays.forEach(( monday => {
-      monday.classList.toggle("selected-day")
+      monday.classList.toggle("selected-day");
       }))
     });
 
   tuesdayInput.addEventListener("click", () => {
     tuesdays.forEach( tuesday => {
-      tuesday.classList.toggle("selected-day")
+      tuesday.classList.toggle("selected-day");
     });
   })
 
@@ -65,14 +66,14 @@
 
   fridayInput.addEventListener("click", ()=> {
     fridays.forEach( friday => {
-      friday.classList.toggle("selected-day")
+      friday.classList.toggle("selected-day");
     })
 
   })
 
   saturdayInput.addEventListener("click", () => {
     saturdays.forEach( saturday => {
-      saturday.classList.toggle("selected-day")
+      saturday.classList.toggle("selected-day");
     })
   })
 
@@ -82,4 +83,155 @@
     })
   })
 
-})();
+};
+
+async function changeMonth(direction){ 
+
+  const year  = Number(document.querySelector("th.month").innerText.replace(/\D/g,""))
+  const month = getMonthFromString(document.querySelector("th.month").innerText.replace(/[^a-z]/gi, ""))
+
+  const date =  {
+    "month"     : month,
+    "year"      : year,
+    "direction" : direction
+  }
+
+
+  const response = await fetch("/changemonth", {
+    "method"       : "POST",
+    "headers"      : {
+      "Content-Type" : "application/json"
+    },
+    "body"         : JSON.stringify(date)
+    }
+  )
+
+  const result = await response.json();
+
+  const calendar = document.querySelector("table.month")
+
+  calendar.innerHTML = result["html"]
+
+
+  const mondayInput    = document.getElementById("monday-input");
+  const tuesdayInput   = document.getElementById("tuesday-input");
+  const wednesdayInput = document.getElementById("wednesday-input");
+  const thurdayInput   = document.getElementById("thurday-input");
+  const fridayInput    = document.getElementById("friday-input");
+  const saturdayInput  = document.getElementById("saturday-input");
+  const sundayInput    = document.getElementById("sunday-input");
+
+
+  const inputs = [mondayInput, tuesdayInput, wednesdayInput, thurdayInput, fridayInput, saturdayInput, sundayInput]
+
+  console.log(inputs)
+
+  changeDayColorBaseOnInput()
+
+  inputs.forEach( input => {
+    if ( input.checked === true ) {
+      day = input.id.split("-")[0]
+      applySelectedDay(day)
+    }
+  })
+
+
+}
+
+
+const next_button = document.getElementById("next");
+
+next_button.addEventListener("click", () => {
+  changeMonth("next");
+})
+
+const prev_button = document.getElementById("prev");
+
+prev_button.addEventListener("click", () => {
+  changeMonth("prev");
+})
+
+const present = document.getElementById("present")
+
+present.addEventListener("click", () =>{
+  changeMonth("present");
+})
+
+
+// Source - https://stackoverflow.com/a/13566675
+// Posted by Aaron Romine, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-07-01, License - CC BY-SA 3.0
+
+function getMonthFromString(mon){
+   return new Date(Date.parse(mon +" 1, 2026")).getMonth()+1
+}
+
+function applySelectedDay(day) {
+
+  const nodeMondays   = document.querySelectorAll(".mon");
+  const nodeTuesadys  = document.querySelectorAll(".tue");
+  const nodeWednesday = document.querySelectorAll(".wed");
+  const nodeThurdays  = document.querySelectorAll(".thu");
+  const nodeFridays   = document.querySelectorAll(".fri");
+  const nodeSaturdays = document.querySelectorAll(".sat");
+  const nodeSundays   = document.querySelectorAll(".sun");
+
+  const mondays    = [...nodeMondays].slice(1);
+  const tuesdays   = [...nodeTuesadys].slice(1);
+  const wednesdays = [...nodeWednesday].slice(1);
+  const thurdays   = [...nodeThurdays].slice(1);
+  const fridays    = [...nodeFridays].slice(1);
+  const saturdays  = [...nodeSaturdays].slice(1);
+  const sundays    = [...nodeSundays].slice(1);
+
+  switch (day) {
+
+    case "monday":
+      mondays.forEach( monday => {
+        monday.classList.add("selected-day");
+      })
+      break;
+
+    case "tuesday":
+      tuesdays.forEach ( tuesday => {
+        tuesday.classList.add("selected-day");
+      })
+      break;
+
+    case "wednesday":
+      wednesdays.forEach ( wednesday => {
+        wednesday.classList.add("selected-day");
+      })
+      break;
+
+    case "thurday":
+      thurdays.forEach ( thurday => {
+        thurday.classList.add("selected-day");
+      })
+      break;
+
+    case "friday":
+      fridays.forEach( friday => {
+        friday.classList.add("selected-day");
+      })
+      break;
+
+    case "saturday":
+      saturdays.forEach( saturday => {
+        saturday.classList.add("selected-day");
+      })
+      break;
+
+    case "sunday":
+      sundays.forEach( sunday => {
+        sunday.classList.add("selected-day");
+      })
+      break;
+
+  }
+
+}
+window.addEventListener("load", (event) => {
+  inputCheck()
+  changeDayColorBaseOnInput()
+});
